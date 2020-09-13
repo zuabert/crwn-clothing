85551bb873ef2e8d84124b69caafea7940acbf85
Denys Abert <denys_abert@epam.com>
2020-09-13 13:39:42 -0400

    setup components tree, directore-menu and menu-item components added

---
+++ src/pages/homepage/homepage.component.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './homepage.styles.scss';
+import DirectoryMenu from '../../components/directory-menu/directory-menu.component';
+
+const HomePage = () => (
+    <div className="homepage">
+        <DirectoryMenu></DirectoryMenu>
+    </div>
+);
+
+export default HomePage;
